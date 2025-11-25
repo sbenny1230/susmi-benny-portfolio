06be2fba14988e40bae6ca5653c6cafbcdd419bd
Susmi Benny <susmibenny1999@gmail.com>
2025-11-25 17:58:29 +0000

    feat: add initial config for project

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
+    index("routes/home/index.tsx"),
     route("experience", "routes/experience.tsx"),
     route("projects", "routes/projects.tsx"),
     route("skills", "routes/skills-and-certifications.tsx")
